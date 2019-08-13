284c347cbdcda920e08f4b64676fdf567bd0b5d9
rosliyanimira <mirarose1302@gmail.com>
2019-08-13 11:32:54 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 Route::get('/tasks','TasksController@index');
 Route::resource('user', 'UserController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
